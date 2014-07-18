e14bfd8c38343d1afdb977ff89b0725a27c3c455
Jason <jasonleedreamer@gmail.com>
2014-07-18 21:02:57 +0800

    Revert "Revert "Revert "test"""
    This reverts commit 0dcf74342569eb7e33909025d6e21600b51118e3.

--- domainClass/Message.java
+++ domainClass/Message.java
@@ -3,72 +3,68 @@ package qin.model.domainClass;
 import java.io.Serializable;
 
 public class Message implements Serializable {
-	
 	private static final long serialVersionUID = 1L;
+	
+	private int sourceId;
+	private int destinationId;
+	private String detail;
+	private boolean isQunMsg;
+	private String dateTime;
+	
+	public Message(){
+		sourceId = 0;
+		destinationId = 0;
+		detail = null;
+		isQunMsg = false;
+		dateTime = null;
+	}
+	
+	public Message(Message msg){
+		sourceId = msg.getSourceId();
+		destinationId = msg.getDestinationId();
+		detail = msg.getDetail();
+		isQunMsg = msg.isQunMsg();
+		dateTime = msg.getDateTime();
+	}
+	
+	public int getSourceId(){
+		return sourceId;
+	}
+	
+	public void setSourceId(int sid){
+		sourceId = sid;
+	}
+	
+	public int getDestinationId(){
+		return destinationId;
+	}
+	
+	public void setDestinationId(int did){
+		destinationId = did;
+	}
+	
+	public String getDetail(){
+		return detail;
+	}
+	
+	public void setDetail(String msg){
+		detail = msg;
+	}
+	
+	public String getDateTime(){
+		return dateTime;
+	}
+	
+	public void setDateTime(String dtime){
+		dateTime = dtime;
+	}
+	
+	public boolean isQunMsg(){
+		return isQunMsg;
+	}
+	
+	public void setIsQunMsg(boolean isqm){
+		isQunMsg = isqm;
+	}
 
-	private int sourceID;
-	
-	// 目的用户ID 或者 目的群ID
-	// 通过 QinMessagePacket.command 来判断具体是哪种
-	private int descriptionID;
-	
-	private String message;
-	
-	// 时间由客户端设定
-	private String time;
-	
-	
-	public Message() {
-		sourceID = 0;
-		descriptionID = 0;
-		message = "";
-		time = "";
-	}
-	
-	
-	public Message(int s, int d, String _message, String _time) {
-		sourceID = s;
-		descriptionID = d;
-		message = _message;
-		time = _time;
-	}
-	
-	
-	public void setSourceID(int s) {
-		sourceID = s;
-	}
-	
-	
-	public void setDescriptionID(int d) {
-		descriptionID = d;
-	}
-	
-	
-	public void setMessage(String _message) {
-		message = _message;
-	}
-	
-	
-	public int getSourceID() {
-		return sourceID;
-	}
-	
-	
-	public int getDescriptionID() {
-		return descriptionID;
-	}
-	
-	public String getMessage() {
-		return message;
-	}
-	
-	
-	public void setTime(String _time) {
-		time = _time;
-	} 
-	
-	
-	public String getTime() {
-		return time;
-	}
 }
